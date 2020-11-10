2dc6e702b9820745175436633962159ce0458e73
Kolby Law <kolbytlaw@gmail.com>
2020-11-10 13:10:15 -0700

    3.2.6 final updates

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,13 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -47,7 +46,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 
     // if player choses to skip
     } else if (promptFight === "skip" || promptFight === "SKIP") {
-        
+
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
   
@@ -68,4 +67,10 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
 }
 
-fight();
+
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
+
+
